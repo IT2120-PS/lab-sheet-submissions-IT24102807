2d2466a4ad6c156158ec1c799eeb44d18f3cb996
IT24102807 <it24102807@my.sliit.lk>
2025-09-16 17:46:54 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102807_Lab_07.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\User\\Desktop\\PS lab\\IT24102807_Lab_07")
+getwd()
+
+#Q1
+# Probability(10 <= X <= 25)
+punif(25,min=0,max=40,lower.tail = TRUE)-punif(10,min=0,max=40,lower.tail = TRUE)
+
+#Q2
+# Probability(X <= 2)
+pexp(2,rate = 1/3,lower.tail = TRUE)
+
+#Q3
+#Part 1
+#IQ > 130 (Normal distribution, mean=100, sd=15)
+1 - pnorm(130,mean = 100,sd = 15, lower.tail = TRUE)
+
+#Part 2
+#95th percentile IQ score
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
+
